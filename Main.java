31550a70399ddd8215d68060d2a77d6884489a70
André Verweij <51133@gmail.com>
2025-02-08 20:20:34 +0100

    test

--- Main.java
+++ Main.java
@@ -236,7 +236,7 @@ public class Main {
         // * position the button */
         gbc.gridx = gridx;
         gbc.gridy = gridy;
-        // * add the button to the window */
+        // * add the button */
         frame.add(num, gbc);
     }
 
